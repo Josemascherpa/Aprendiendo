1740c4c61300ff6a7d68ea1eea3e42764813b6bd
Josemascherpa <joseimascherpa@gmail.com>
2023-01-09 18:50:36 -0300

    Animaciones de atacar

--- Assets/PlayerScript.cs
+++ Assets/PlayerScript.cs
@@ -11,8 +11,9 @@ public class PlayerScript : MonoBehaviour
     private Animator anim;
     private bool run = false;
     private bool jump = false;
+    private bool attack1 = false;
+    private bool attack2 = false;
     [SerializeField]private float jumpSpeed = 5f;
-
 
     private Vector3 velocity;
     void Start()
@@ -22,16 +23,52 @@ public class PlayerScript : MonoBehaviour
 
     }
     private void Update()
-    {       
-        if (Input.GetKeyDown(KeyCode.Space) && !jump)
-        {           
-            jump = true;            
-            rb.AddForce(new Vector3(0f, jumpSpeed, 0f), ForceMode.Impulse);
+    {
+        if (attack1)
+        {
+            speed = 10;
         }
-        anim.SetBool("Run Forward", run);
-        anim.SetBool("Jump", jump);
+        else
+        {
+            speed = 5;
+        }
+        Inputs();
+        Animation();
     }
     private void FixedUpdate()
+    {
+        Movement();
+    }
+    void Inputs()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && !jump)
+        {
+            jump = true;
+            rb.AddForce(new Vector3(0f, jumpSpeed, 0f), ForceMode.Impulse);
+        }
+        
+        if (Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            attack1 = true;
+            anim.Play("Attack1");
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftAlt))
+        {
+            attack2 = true;
+            anim.Play("Attack2");//TERMINAR ATTACK2 Y ATTACK 1 poner mas vel
+        }
+    }
+
+    void Animation()
+    {
+        anim.SetBool("Run Forward", run);
+        anim.SetBool("Jump", jump);
+        anim.SetBool("Attack1", attack1);
+        anim.SetBool("Attack2", attack2);
+    }
+
+    void Movement()
     {
         float hor = Input.GetAxisRaw("Horizontal");
         float ver = Input.GetAxisRaw("Vertical");
@@ -40,12 +77,12 @@ public class PlayerScript : MonoBehaviour
             velocity = Vector3.zero;
         }
 
-        if (hor != 0.0f || ver != 0.0f )
+        if (hor != 0.0f || ver != 0.0f)
         {
             if (!jump)
             {
                 run = true;
-            }            
+            }
             Vector3 dir = transform.forward * ver + transform.right * hor;
             velocity = dir.normalized * speed;
             Vector3 rotateForward = transform.position - Camera.main.transform.position;
@@ -53,17 +90,17 @@ public class PlayerScript : MonoBehaviour
             rotation.x = 0;
             rotation.z = 0;
             transform.rotation = Quaternion.Slerp(transform.rotation, rotation, 10 * Time.deltaTime);
-            velocity.y = rb.velocity.y;
-            rb.velocity = velocity ;
+
         }
         else
         {
             run = false;
         }
-
-        if (rb.velocity.y >= 7)
+        velocity.y = rb.velocity.y;
+        rb.velocity = velocity;        
+        if (rb.velocity.y >= jumpSpeed)
         {
-            rb.velocity = new Vector3(rb.velocity.x, 7f, rb.velocity.z);
+            rb.velocity = new Vector3(rb.velocity.x, jumpSpeed, rb.velocity.z);
         }
     }
 
@@ -77,5 +114,13 @@ public class PlayerScript : MonoBehaviour
                 anim.Play("Idle");
             }
         }
+    }
+
+    void Attack01False()
+    {
+        attack1 = false;
+    }void Attack02False()
+    {
+        attack2 = false;
     }
 }
